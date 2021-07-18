5f694903f14c50067015e9caebfa3b41feb38a40
Kosuke <bkpc9070@yahoo.ne.jp>
2021-07-18 23:53:55 +0900

    タイトルロゴを作成しました

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,22 @@
 <template>
-    <div class="hello">
-        こんばんは
+    <div>
+        <h1 class="title">山口県 登山MAP</h1>
+        <!-- ここにGooglemapを表示 -->
+        <div id="map"></div>
     </div>
+    
 </template>
+
+<style>
+.title {
+    text-align: center;
+}
+</style>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
